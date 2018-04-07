a326b9a568cb353ec0831aaf67ae139bf6eeea40
Alexei Gontar <f.gontar5@gmail.com>
2018-04-07 16:14:34 +0300

    Updated install script version.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
-wget https://github.com/Mamoru1234/dspm/releases/download/0.0.8/dspm.tar.gz
+DSPM_VERSION="0.1.1"
+
+echo "Installing dspm version $DSPM_VERSION"
+
+wget "https://github.com/Mamoru1234/dspm/releases/download/$DSPM_VERSION/dspm.tar.gz"
 
 mkdir -p .dspm/dist
 tar -xzf dspm.tar.gz -C .dspm/dist
