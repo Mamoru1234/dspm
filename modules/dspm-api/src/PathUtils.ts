5153ba351273c327357400efc071ddcb1202a59a
Alexei Gontar <f.gontar5@gmail.com>
2018-10-17 19:11:09 +0300

    Postponed modularity of dspm.

--- modules/dspm-api/src/PathUtils.ts
+++
@@ -1,8 +0,0 @@
-import {isAbsolute, join} from 'path';
-
-export function normalizePath(basePath: string, targetPath: string): string {
-  if (isAbsolute(targetPath)) {
-    return targetPath;
-  }
-  return join(basePath, targetPath);
-}
